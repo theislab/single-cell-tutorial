5f7f3d799e20666cc9a501d23b6b5ba4256a79a6
LuckyMD <m.d.luecken@gmail.com>
2019-03-15 14:19:50 +0100

    added plotting script for extended view figure 1, the HVG plot

---
+++ plotting_scripts/plot_hvgs.r
@@ -0,0 +1,11 @@
+library(ggplot2)
+
+data <- read.table("../HVGs.csv", header=T, sep=';')
+data$Log.Cell.Number = log(data$Cells, 10)
+
+image=ggplot(data, aes(x=Log.Cell.Number, y=HVGs, color=Technology.type)) +
+geom_point() +
+labs(x="Log 10 Number of Cells", y="Number of HVGs", color="Technology type") +
+theme_bw()
+
+ggsave("HVG_plot.svg", plot=image)
